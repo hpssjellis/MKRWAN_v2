48a10b683799b394856b7fbf7af73ead4beef6fe
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-17 14:39:09 -0700

    Create at-command-serial.ino

---
+++ examples/at-command-serial/at-command-serial.ino
@@ -0,0 +1,11 @@
+  
+/*
+  First Configuration
+  This sketch demonstrates the usage of MKR WAN 1300/1310 LoRa module.
+  This example code is in the public domain.
+*/
+
+#include <MKRWAN_v2.h>
+
+LoRaModem modem;
+
